f262accabe6f93f9f49a0e67b5ff793743688dbf
Alperen Bozkurt <78342156+Hadacar@users.noreply.github.com>
2023-11-28 16:52:23 +0300

    updated

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
-import Modal from "./components/modals/Modal";
 import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./providers/ToasterProvider";
 import LoginModal from "./components/modals/LoginModal";
